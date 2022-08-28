7399d420279d0b464b4f9653405ac9ab6189995d
Naveen Reddy <50566786+Naveen-reddy91@users.noreply.github.com>
2022-08-28 12:33:09 +0530

    Add files via upload

---
+++ Programming Assignment 1.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Write a Python program to print Hello Python?
+
+# In[2]:
+
+
+message = "Hello Python"
+
+print(message)
+
+
+# 2. Write a Python program to do arithmetical operations addition and division.?
+
+# In[19]:
+
+
+a = 10
+b = 5
+
+print(a+b)
+print(a/b)
+
+
+# 3. Write a Python program to find the area of a triangle?
+
+# In[8]:
+
+
+b = int(input("Enter the base: "))
+h = int(input("Enter the height: "))
+
+area = 1/2*(b*h)
+
+print("Area of Triangle is:", area)
+
+
+# 4. Write a Python program to swap two variables?
+
+# In[9]:
+
+
+x = 4
+y = 8
+
+x,y = y, x
+
+print("x ", x)
+print("y ", y)
+
+
+# 5. Write a Python program to generate a random number?
+
+# In[14]:
+
+
+import random
+
+print(random.randint(0, 10))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
